f12078e36af6b3b18e7ad4b2808ad21273e8b031
liikii <609957891@qq.com>
2018-12-05 11:20:29 +0800

    u

--- strtest/main.rs
+++ strtest/main.rs
@@ -1,5 +1,9 @@
 fn main() {
-	let mut s = String::from("hello");
-	s.push_str(", world!"); // push_str() appends a literal to a String
-	println!("{}", s); // This will print `hello, world!`
+	// let mut s = String::from("hello");
+	// s.push_str(", world!"); // push_str() appends a literal to a String
+	// println!("{}", s); // This will print `hello, world!`
+	let x = 5;
+	let y = x;
+
+	println!("x = {}, y = {}", x, y);
 }
